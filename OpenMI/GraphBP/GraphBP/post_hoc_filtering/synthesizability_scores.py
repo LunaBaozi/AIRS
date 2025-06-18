aac87833ec9a0f89f54df21d0e866e65ce705429
LunaBaozi <luna.pianesi@gmail.com>
2025-06-18 06:38:22 +0000

    add wrapper for aurk files and modify synth file

--- OpenMI/GraphBP/GraphBP/post_hoc_filtering/synthesizability_scores.py
+++ OpenMI/GraphBP/GraphBP/post_hoc_filtering/synthesizability_scores.py
@@ -11,6 +11,7 @@ from syba.syba import SybaClassifier
 
 
 from scripts import scscorer_standalone 
+from scripts.aurk_int_preprocess import read_aurora_kinase_interactions
 
 
 # Initialize SCScorer
@@ -64,6 +65,16 @@ def calculate_sa_score(mol):
     """
     return sascorer.calculateScore(mol) 
 
+def calculate_sc_score(smi):
+    """
+    Calculates the Synthetic Accessibility Score (SAS) for a given molecule.
+    Args:
+        mol (rdkit.Chem.rdchem.Mol): The RDKit molecule object.
+    Returns:
+        float: The SAS score of the molecule.
+    """
+    return scscorer.get_score_from_smi(smi)
+
 
 def calculate_np_score(mol):
     """
@@ -77,70 +88,89 @@ def calculate_np_score(mol):
     score = npscorer.scoreMol(mol, fscore)
     confidence = npscorer.scoreMolWConfidence(mol, fscore)
     return score, confidence
-    
-    
 
-def calculate_scores_to_csv(folder_path, csv_path):
+
+def calculate_syba_score(smi):
     """
-    Calculates scores for all SDF files in a folder and writes them to a CSV file using Pandas.
-
+    Calculates the Synthetic Accessibility Score (SAS) for a given molecule.
     Args:
-        folder_path (str): Path to the folder containing SDF files.
-        csv_path (str): Path to the output CSV file.
+        mol (rdkit.Chem.rdchem.Mol): The RDKit molecule object.
+    Returns:
+        float: The SAS score of the molecule.
     """
-    data = []
-    idx = 0
+    return syba.predict(smi)
+
+
+def load_mols_from_sdf_folder(folder_path):
+    mols = []
+    smiles = []
+    filenames = []
     for filename in os.listdir(folder_path):
         if filename.endswith('.sdf'):
-            sdf_path = os.path.join(folder_path, filename)
-            suppl = Chem.SDMolSupplier(sdf_path)
-            for mol in suppl:
-                if mol is not None:
-                    sa_score = calculate_sa_score(mol)
-                    np_score, _ = calculate_np_score(mol)
-                    smi = Chem.MolToSmiles(mol)
-                    (smi, sc_score) = scscorer.get_score_from_smi(smi)
-                    syba_score = syba.predict(smi)
-                    data.append({
-                        'filename': filename,
-                        'index': idx,
-                        'smiles': smi,
-                        'len_smiles': len(smi),
-                        'SA_score': sa_score,
-                        'NP_score': np_score,
-                        'SCScore': sc_score,
-                        'Syba_score': syba_score
-                    })
-                    idx += 1
-                break  # Only process the first molecule in each file
-    df = pd.DataFrame(data)
-    # Reorder columns to have 'filename' as the first column
-    cols = ['filename'] + [col for col in df.columns if col != 'filename']
-    df = df[cols]
-    # Ensure the output directory exists
-    os.makedirs(os.path.dirname(csv_path), exist_ok=True)
-    df.to_csv(csv_path, index=False)
+            mol = Chem.SDMolSupplier(os.path.join(folder_path, filename))[0]
+            if mol is not None:
+                mols.append(mol)
+                smiles.append(Chem.MolToSmiles(mol))
+                filenames.append(filename)
+    return mols, smiles, filenames
+    
+
+def calculate_scores(mols, smiles, filenames):
+    results = []
+    for mol, smi, fn in zip(mols, smiles, filenames):
+        if mol is None:
+            continue
+        sa_score = calculate_sa_score(mol)
+        np_score, _ = calculate_np_score(mol)
+        (smi, sc_score) = calculate_sc_score(smi)
+        syba_score = calculate_syba_score(smi)
+        results.append({
+            'filename': fn,
+            'smiles': smi,
+            'len_smiles': len(smi),
+            'SA_score': sa_score,
+            'SCScore': sc_score,
+            'NP_score': np_score,
+            'Syba_score': syba_score
+        })
+    return pd.DataFrame(results)
+
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Calculate synthesizability scores for generated molecules.")
-    parser.add_argument('--epoch', type=int, required=True, help='Epoch number')
-    parser.add_argument('--num_gen', type=int, required=True, help='Number of molecules generated')
-    parser.add_argument('--known_binding_site', type=str2bool, required=True, help='Known binding site (True/False)')
+    parser = argparse.ArgumentParser(description="Wrapper for AIRS pipeline.")
+    parser.add_argument('--num_gen', type=int, required=False, default=0, help='Number of generations')
+    parser.add_argument('--epoch', type=int, required=False, default=0, help='Epoch number')
+    parser.add_argument('--known_binding_site', type=str, required=False, default='0', help='Known binding site')
+    parser.add_argument('--aurora', type=str, required=True, help='Aurora kinase')
     args = parser.parse_args()
 
     num_gen = args.num_gen
     known_binding_site = args.known_binding_site
     epoch = args.epoch
+    aurora = args.aurora
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
     sdf_folder = os.path.join(parent_dir, f"trained_model_reduced_dataset_100_epochs/gen_mols_epoch_{epoch}_mols_{num_gen}_bs_{known_binding_site}/sdf")
-    results_dir = os.path.join(script_dir, f"results_epoch_{epoch}_mols_{num_gen}_bs_{known_binding_site}")
-    csv_path = os.path.join(results_dir, f"molecule_scores_{epoch}_{num_gen}.csv")
-    calculate_scores_to_csv(
-        sdf_folder,
-        csv_path
-    )
+    known_inhib_file = os.path.join(script_dir, f"data/aurora_kinase_{aurora}_interactions.csv")
+    results_dir = os.path.join(script_dir, f"results_epoch_{epoch}_mols_{num_gen}_bs_{known_binding_site}_aurora_{aurora}")
+    csv_path = os.path.join(results_dir, f"synthesizability_scores_{epoch}_{num_gen}_{known_binding_site}_{aurora}.csv")
+    
+    os.makedirs(os.path.dirname(csv_path), exist_ok=True)
+    
+    if epoch != 0:
+        # Calculating scores for generated molecules
+        mols, smiles, filenames = load_mols_from_sdf_folder(sdf_folder)
+        synth = calculate_scores(mols, smiles, filenames)
+        synth.to_csv(csv_path, index=False)
+
+    else:
+        # Calculating scores for Aurora inhibitors
+        mols, smiles, filenames, fps = read_aurora_kinase_interactions(known_inhib_file)
+        synth = calculate_scores(mols, smiles, filenames)
+        synth.to_csv(csv_path, index=False)
+
 
 
 # NOTE: The SA_Score ranges from 1 to 10 with 1 being easy to make and 10 being hard to make.
---
+++ OpenMI/GraphBP/GraphBP/wrapper_known_inhibitors.py
@@ -0,0 +1,60 @@
+import argparse
+import subprocess
+import os, sys, csv, time
+
+
+def run_script(script_path, args=None):
+    cmd = [sys.executable, script_path]
+    if args:
+        cmd += args
+    subprocess.run(cmd, check=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Wrapper for CADD pipeline targeted to Aurora protein kinases.")
+    parser.add_argument('--num_gen', type=int, required=False, default=0, help='Desired number of generated molecules (int, positive)')
+    parser.add_argument('--epoch', type=int, required=False, default=0, help='Epoch number the model will use to generate molecules (int, 0-99)')
+    parser.add_argument('--known_binding_site', type=str, required=False, default='0', help='Allow model to use binding site information (True, False)')
+    parser.add_argument('--aurora', type=str, required=True, help='Aurora kinase type (str, A, B)')
+    args = parser.parse_args()
+
+    # Paths
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    post_hoc_dir = os.path.join(base_dir, 'post_hoc_filtering')
+    scripts = [
+        os.path.join(post_hoc_dir, 'synthesizability_scores.py'),
+        # os.path.join(post_hoc_dir, 'tanimoto_inter.py'),
+        # os.path.join(post_hoc_dir, 'tanimoto_intra.py'),
+        # os.path.join(post_hoc_dir, 'lipinski.py'),
+        # os.path.join(post_hoc_dir, 'filtering.py'),
+    #     os.path.join(post_hoc_dir, 'post_processed_graphics.py'),
+    ]
+
+    # Arguments to pass to main_gen.py and main_eval.py
+    param_args = [
+        '--num_gen', str(args.num_gen),
+        '--epoch', str(args.epoch),
+        '--known_binding_site', str(args.known_binding_site),
+        '--aurora', str(args.aurora).upper()
+    ]
+
+    # Run analyses and measure execution time
+    start_time = time.time()
+
+    results_dir = os.path.join(
+        base_dir,
+        "post_hoc_filtering",
+        f"results_epoch_{args.epoch}_mols_{args.num_gen}_bs_{args.known_binding_site}_aurora_{args.aurora}"
+    )
+    os.makedirs(results_dir, exist_ok=True)
+    
+    # Run post-hoc filtering scripts (no extra args)
+    for script in scripts:
+        run_script(script, param_args)
+
+    end_time = time.time() - start_time
+    # print(f"main_gen.py executed in {elapsed_time:.2f} seconds")
+    print(f"Whole pipeline executed in {end_time:.2f} seconds")
+
+if __name__ == '__main__':
+    main()
